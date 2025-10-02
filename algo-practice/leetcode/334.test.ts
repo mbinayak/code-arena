60dc24739acff65240899d3a202869bc7bcb567c
Binayak Mishra <mbinayak@ymail.com>
2025-10-02 18:49:58 +0300

    leetcode p.334 solution with specs

---
+++ algo-practice/leetcode/334.test.ts
@@ -0,0 +1,15 @@
+import increasingTriplet from "./334";
+
+describe("Incresing Triplet", () => {
+  test("Test case 1", () => {
+    expect(increasingTriplet([1, 2, 3, 4, 5])).toBe(true);
+  });
+
+  test("Test case 2", () => {
+    expect(increasingTriplet([5, 4, 3, 2, 1])).toBe(false);
+  });
+
+  test("Test case 3", () => {
+    expect(increasingTriplet([2, 1, 5, 0, 4, 6])).toBe(true);
+  });
+});
---
+++ algo-practice/leetcode/334.ts
@@ -0,0 +1,31 @@
+function increasingTriplet(nums: number[]): boolean {
+  const l = nums.length;
+  const minArray = new Array(l).fill(1);
+  const maxArray = new Array(l).fill(0);
+  minArray[0] = nums[0];
+  maxArray[l - 1] = nums[l - 1];
+  for (let i = 1, j = l - 2; i < l && j >= 0; i++, j--) {
+    minArray[i] = minArray[i - 1];
+    if (minArray[i] > (nums[i] as number)) {
+      minArray[i] = nums[i];
+    }
+
+    maxArray[j] = maxArray[j + 1];
+    if (maxArray[j] < (nums[j] as number)) {
+      maxArray[j] = nums[j];
+    }
+  }
+
+  for (let i = 1; i <= l - 1; i++) {
+    if (
+      minArray[i - 1] < (nums[i] as number) &&
+      (nums[i] as number) < maxArray[i + 1]
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export default increasingTriplet;
